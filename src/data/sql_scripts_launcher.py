53de951faa9310153c37abc5049b5c0e80bfaf77
Clément Richard <c.richard@sstrn.com>
2024-05-13 15:21:13 +0200

    add more debug

--- src/data/sql_scripts_launcher.py
+++ src/data/sql_scripts_launcher.py
@@ -1,6 +1,5 @@
 import os
-from sqlalchemy import create_engine
-from sqlalchemy import text
+from sqlalchemy import create_engine, text
 from sqlalchemy_utils import database_exists, create_database
 
 def execute_sql_script(script_path: str, engine: create_engine):
@@ -14,11 +13,10 @@ def execute_sql_script(script_path: str, engine: create_engine):
     try:
         with open(script_path, 'r') as script_file:
             sql_script = text(script_file.read())
-            print(f"SQL script as text: {sql_script}")
+            print(f"Executing SQL script: {script_path}")
             with engine.connect() as connection:
-                print(f"Executing SQL script: {script_path}")
                 connection.execute(sql_script)
-                print(f"SQL script execution completed: {script_path}")
+            print(f"SQL script execution completed: {script_path}")
     except Exception as e:
         print(f"Error executing SQL script {script_path}: {e}")
 
@@ -39,34 +37,39 @@ def main():
         f"{db_config['dbms_ip']}:{db_config['dbms_port']}/{db_config['dbms_database']}"
     )
     
-    # Create the database if it doesn't exist
-    if not database_exists(database_url):
-        create_database(database_url)
-        print(f"Database '{db_config['dbms_database']}' created successfully.")
-    else:
-        print(f"Database '{db_config['dbms_database']}' already exists.")
-    
-    # Creating SQLAlchemy engine
-    engine = create_engine(database_url)
-    
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    # Construct the relative path to the folder
-    
-    print(f'script dir: {script_dir}')
-    
-    folder_path = os.path.join(script_dir, '..', 'script')
-    
-    # Get all SQL script files in the directory
-    sql_scripts = [f for f in os.listdir(folder_path) if f.lower().endswith('.sql') and os.path.isfile(os.path.join(folder_path, f))]
-    
-    # Execute each SQL script
-    for script_file in sql_scripts:
-        script_path = os.path.join(folder_path, script_file)
-        print(f"Executing script: {script_file}")
-        execute_sql_script(script_path, engine)
-        print("Script execution completed.")
-    
-    print("All scripts executed successfully.")
+    try:
+        # Check if the database exists
+        if not database_exists(database_url):
+            # Create the database if it doesn't exist
+            create_database(database_url)
+            print(f"Database '{db_config['dbms_database']}' created successfully.")
+        else:
+            print(f"Database '{db_config['dbms_database']}' already exists.")
+        
+        # Check database access rights
+        engine = create_engine(database_url)
+        with engine.connect():
+            print("Database connection successful.")
+            
+            # Fetch the SQL scripts directory path
+            script_dir = os.path.dirname(os.path.abspath(__file__))
+            folder_path = os.path.join(script_dir, '..', 'script')
+            
+            # Check if the directory containing SQL scripts exists
+            if os.path.exists(folder_path):
+                # Get all SQL script files in the directory
+                sql_scripts = [f for f in os.listdir(folder_path) if f.lower().endswith('.sql') and os.path.isfile(os.path.join(folder_path, f))]
+                
+                # Execute each SQL script
+                for script_file in sql_scripts:
+                    script_path = os.path.join(folder_path, script_file)
+                    print(f"Executing script: {script_file}")
+                    execute_sql_script(script_path, engine)
+                    print("Script execution completed.")
+            else:
+                print("Directory containing SQL scripts not found.")
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == '__main__':
     main()
